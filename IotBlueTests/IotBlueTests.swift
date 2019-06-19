149f8616ddb88fcb0e237c61978c49bf9caab8c9
mohamed nagi <mohamed_nagi_93@hotmail.com>
2019-06-19 12:55:23 +0200

    Initial Commit

---
+++ IotBlueTests/IotBlueTests.swift
@@ -0,0 +1,36 @@
+//
+//  IotBlueTests.swift
+//  IotBlueTests
+//
+//  Created by Sierra on 6/19/19.
+//  Copyright © 2019 Nagiz. All rights reserved.
+//
+
+import XCTest
+@testable import IotBlue
+
+class IotBlueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
